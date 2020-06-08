88b1a972f6685b04b77b0855720fd5533624e361
Kevin.Singer <kevin.singer@boeing.com>
2020-06-07 18:32:42 -0700

    interate task queue

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const mkdirp = require('mkdirp');
 const path = require('path');
 const utilities = require('./utilities');
 const TaskQueue = require('./taskQueue');
-let downloadQueue = new TaskQueue(2);
+let downloadQueue = new TaskQueue(5);
 
 function spiderLinks(currentUrl, body, nesting, callback) {
   if(nesting === 0) {
@@ -18,15 +18,66 @@ function spiderLinks(currentUrl, body, nesting, callback) {
     return process.nextTick(callback);
   }
 
+  // let completed = 0, hasErrors = false;
+  // links.forEach(link => {
+  //   downloadQueue.pushTask(done => {
+  //     spider(link, nesting - 1, err => {
+  //       if(err) {
+  //         hasErrors = true;
+  //         return callback(err);
+  //       }
+  //       if(++completed === links.length && !hasErrors) {
+  //         callback();
+  //       }
+  //       done();
+  //     });
+  //   });
+  // });
+}
+
+function saveFile(filename, contents, callback) {
+  console.log(filename);
+  callback(null, contents, callback);
+}
+let spidering = new Map();
+function download(url, nesting, callback) {
+  if(spidering.has(url)) {
+    return process.nextTick(callback);
+  }
+  spidering.set(url, true);
+  console.log(`Downloading ${url}`);
+  request(url, (err, response, body) => {
+    if(err) {
+      return callback(err);
+    }
+    console.log(`Downloaded and saved: ${url}`);
+    return callback();
+  });
+}
+
+
+function spider(url, nesting, callback) {
+  let kev = ["http://www.google.com",
+    "https://www.google.com/calendar?tab=wc",
+    "https://www.google.com",
+    "http://www.google.com/finance?tab=we",
+    "https://www.google.com/intl/en/about/products?tab=wh"];
+  // kev.forEach(download)
+
+  // if(kev.length === 0) {
+  //   return process.nextTick(callback);
+  // }
+
   let completed = 0, hasErrors = false;
-  links.forEach(link => {
+
+  kev.forEach(link => {
     downloadQueue.pushTask(done => {
-      spider(link, nesting - 1, err => {
+      download(link, nesting - 1, err => {
         if(err) {
           hasErrors = true;
           return callback(err);
         }
-        if(++completed === links.length && !hasErrors) {
+        if(++completed === kev.length && !hasErrors) {
           callback();
         }
         done();
@@ -35,58 +86,7 @@ function spiderLinks(currentUrl, body, nesting, callback) {
   });
 }
 
-function saveFile(filename, contents, callback) {
-  mkdirp(path.dirname(filename), err => {
-    if(err) {
-      return callback(err);
-    }
-    fs.writeFile(filename, contents, callback);
-  });
-}
-
-function download(url, filename, callback) {
-  console.log(`Downloading ${url}`);
-  request(url, (err, response, body) => {
-    if(err) {
-      return callback(err);
-    }
-    saveFile(filename, body, err => {
-      if(err) {
-        return callback(err);
-      }
-      console.log(`Downloaded and saved: ${url}`);
-      callback(null, body);
-    });
-  });
-}
-
-let spidering = new Map();
-function spider(url, nesting, callback) {
-  if(spidering.has(url)) {
-    return process.nextTick(callback);
-  }
-  spidering.set(url, true);
-
-  const filename = utilities.urlToFilename(url);
-  fs.readFile(filename, 'utf8', function(err, body) {
-    if(err) {
-      if(err.code !== 'ENOENT') {
-        return callback(err);
-      }
-
-      return download(url, filename, function(err, body) {
-        if(err) {
-          return callback(err);
-        }
-        spiderLinks(url, body, nesting, callback);
-      });
-    }
-
-    spiderLinks(url, body, nesting, callback);
-  });
-}
-
-spider(process.argv[2], 1, (err) => {
+spider(process.argv[2], 3, (err) => {
   if(err) {
     console.log(err);
     process.exit();
